9de06f60a86819745c48c6c71ea1f79e6d0bd71d
kamwerunaomi <kamwerunaomi@gmail.com>
2017-05-09 15:23:08 +0300

    This is code that outputs fizz if a number is divisible by 3 outputs buzz if its divisible by 5 outputs fizzbuzz if its divisible by both 3 and 5 and returns the number if its not divisble by either

---
+++ fizz.py
@@ -0,0 +1,16 @@
+class Fizzbuzz():
+	def fizzbuzz(numbers):
+		for x in numbers:
+			if x%3==0 and x%5==0:
+				print("fizzbuzz")
+			elif x%3==0:
+				print("fizz")
+			elif x%5==0:
+				print("buzz")
+			else:
+				print (x)
+
+
+
+
+			
